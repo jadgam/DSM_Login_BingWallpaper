769fde9eb2687638397884af98dcc2ab6c02e51d
jadgam <32200656+jadgam@users.noreply.github.com>
2020-02-25 19:36:33 +0800

    Update ChangeBing.sh

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,3 +1,5 @@
+#原项目地址：https://github.com/kkkgo/DSM_Login_BingWallpaper
+#Get_BingWallpaper(修改：jadgam 20200225)
 #如需收集每日美图去掉下面注释设置保存文件夹路径
 #savepath="/volume1/wallpaper"
 #在FileStation里面右键文件夹属性可以看到路径
@@ -8,6 +10,11 @@ date=$(echo $pic|sed 's/.\+enddate[": ]\+//g'|grep -Eo 2[0-9]{7}|head -1)
 tmpfile=/tmp/$date"_bing.jpg"
 wget -t 5 --no-check-certificate  $link -qO $tmpfile
 [ -s $tmpfile ]||exit
+#替换桌面背景图片
+rm -rf /usr/syno/etc/preference/admin/wallpaper
+cp -f $tmpfile /usr/syno/etc/preference/admin/wallpaper &>/dev/null
+sed -i s/customize_wallpaper\":false/customize_wallpaper\":true/ /usr/syno/etc/preference/admin/usersettings
+#替换登录背景图片
 rm -rf /usr/syno/etc/login_background*.jpg
 cp -f $tmpfile /usr/syno/etc/login_background.jpg &>/dev/null
 cp -f $tmpfile /usr/syno/etc/login_background_hd.jpg &>/dev/null
@@ -19,11 +26,11 @@ cninfo=$(echo $copyright|sed 's/，/"/g'|sed 's/,/"/g'|sed 's/(/"/g'|sed 's/ //g
 title=$(echo $cninfo|cut -d'"' -f1)
 word=$(echo $cninfo|cut -d'"' -f2)
 fi
-sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
+sed -i /login_background_customize=.*/d /etc/synoinfo.conf
+sed -i /login_welcome_title=.*/d /etc/synoinfo.conf
+sed -i /login_welcome_msg=.*/d /etc/synoinfo.conf
 echo "login_background_customize=\"yes\"">>/etc/synoinfo.conf
-sed -i s/login_welcome_title=.*//g /etc/synoinfo.conf
 echo "login_welcome_title=\"$title\"">>/etc/synoinfo.conf
-sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
 echo "login_welcome_msg=\"$word\"">>/etc/synoinfo.conf
 if (echo $savepath|grep -q '/') then
 cp -f $tmpfile $savepath/$date@$title-$word.jpg
